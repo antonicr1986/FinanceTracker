56bbd4754f69e0e5e6c237c054849e19013a043b
antonicr1986 <antonicr1986@hotmail.com>
2026-05-18 19:57:48 +0200

    Add transaction DTOs

---
+++ FinanceTracker.Application/DTOs/Transactions/TransactionDto.cs
@@ -0,0 +1,26 @@
+﻿using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Application.DTOs.Transactions;
+
+public class TransactionDto
+{
+    public int Id { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Amount { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public TransactionType Type { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = string.Empty;
+}
---
+++ FinanceTracker.Application/DTOs/Transactions/UpdateTransactionDto.cs
@@ -0,0 +1,22 @@
+﻿using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Application.DTOs.Transactions;
+
+public class UpdateTransactionDto
+{
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Amount { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public TransactionType Type { get; set; }
+
+    public int CategoryId { get; set; }
+}
