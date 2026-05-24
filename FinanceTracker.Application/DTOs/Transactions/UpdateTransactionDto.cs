55ab9d541c50cc19360ea007b490355a28d1279c
antonicr1986 <antonicr1986@hotmail.com>
2026-05-25 01:34:50 +0200

    Add transaction DTO validation

--- FinanceTracker.Application/DTOs/Transactions/UpdateTransactionDto.cs
+++ FinanceTracker.Application/DTOs/Transactions/UpdateTransactionDto.cs
@@ -1,5 +1,4 @@
-﻿using FinanceTracker.Domain.Entities;
-using FinanceTracker.Domain.Enums;
+﻿using FinanceTracker.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace FinanceTracker.Application.DTOs.Transactions;
@@ -7,18 +6,16 @@ namespace FinanceTracker.Application.DTOs.Transactions;
 public class UpdateTransactionDto
 {
     [Required]
-    [MaxLength(100)]
+    [MaxLength(150)]
     public string Description { get; set; } = string.Empty;
 
-    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0.")]
     public decimal Amount { get; set; }
 
-    [Required]
     public DateTime Date { get; set; }
 
-    [Required]
     public TransactionType Type { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than 0")]
+    [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than 0.")]
     public int CategoryId { get; set; }
 }
