aca33c0b74938981f7504db20e06e479becb2b66
antonicr1986 <antonicr1986@hotmail.com>
2026-05-31 12:36:37 +0200

    Add users database migration

---
+++ FinanceTracker.Infraestructure/Migrations/20260531103052_AddUsers.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Transactions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Budgets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Budgets_Users_UserId",
+                table: "Budgets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Budgets_Users_UserId",
+                table: "Budgets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Budgets");
+        }
+    }
+}
